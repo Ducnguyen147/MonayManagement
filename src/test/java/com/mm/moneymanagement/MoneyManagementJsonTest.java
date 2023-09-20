15724190c1eddc013a05b771f1f772761e73174c
Duc <nguyenanhduc9a1@gmail.com>
2023-09-20 21:06:41 +0200

    Initialize the application

---
+++ src/test/java/com/mm/moneymanagement/MoneyManagementJsonTest.java
@@ -0,0 +1,13 @@
+package com.mm.moneymanagement;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MoneyManagementJsonTest {
+
+    @Test
+    public void firstTest(){
+        assertThat(42).isEqualTo(42);
+    }
+    
+}
